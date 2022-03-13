4942c426e9f4831f386e73fd0e1ffa9667521b89
carlosHorita <horita.cicunb@gmail.com>
2022-03-13 17:53:29 -0300

    Implementação do Resource

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/enums/ResourceType.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dslearnbds.entities.enums;
+
+public enum ResourceType {
+
+	//lesson_only - 0, Lesson_task - 1... etc
+	LESSON_ONLY,
+	LESSON_TASK,
+	FORUM,
+	EXTERNAL_LINK;
+}
